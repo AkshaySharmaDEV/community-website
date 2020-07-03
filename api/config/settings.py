9b2800b4c0cda9cd4b39788a1e67c1fbe123ebf2
rohitgeddam <rohitgeddam2018@gmail.com>
2020-07-03 09:41:38 +0530

    changed default db to sqlite for development

--- api/config/settings.py
+++ api/config/settings.py
@@ -84,7 +84,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'devdb',
+    },
+    "postgres": {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
         "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
         "USER": os.environ.get("SQL_USER", "user"),
